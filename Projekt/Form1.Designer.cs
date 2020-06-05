899996666bcd1dd7d52b01a90531e8d3bc661fed
YahorKazak <50490788+YahorKazak@users.noreply.github.com>
2020-06-05 21:40:16 +0200

    Initial commit

---
+++ Projekt/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Projekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(626, 452);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Bisque;
+            this.button1.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(632, 376);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 52);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(632, 247);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(170, 22);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Algorytm Bresenhama";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Baskerville Old Face", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(632, 199);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(167, 22);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Algorytm Przyrostowy";
+            this.radioButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Projekt/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Imaging;
+
+
+namespace Projekt
+{
+    public partial class Form1 : Form
+    {
+        Bitmap m;     
+        int X, Y, X1, Y1;
+        bool Cleaked = false;
+        bool AlgorytmBresenhama;
+        bool AlgorytmPrzyrostowy;
+        public Form1()
+        {
+            InitializeComponent();
+            m = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Cleaked = true;
+            X = e.X;
+            Y = e.Y;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            m.Dispose();
+            m = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            X = 0;
+            Y = 0;
+            X1 = 0;
+            Y1 = 0;
+            if (AlgorytmBresenhama == true)
+            {
+                AlgorytmBresenhama = true;
+            }
+            else
+            {
+                AlgorytmBresenhama = false;
+                AlgorytmPrzyrostowy = true;
+            }
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            AlgorytmBresenhama = true;
+            AlgorytmPrzyrostowy = false;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Cleaked == true)
+            {
+                X1 = e.X;
+                Y1 = e.Y;
+                if (X1 < 0)
+                {
+                    X1 = 0;
+                }
+                if (Y1 < 0)
+                {
+                    Y1 = 0;
+                }
+                if (X1 > pictureBox1.Width)
+                {
+                    X1 = pictureBox1.Width - 1;
+                }
+                if (Y1 > pictureBox1.Height)
+                {
+                    Y1 = pictureBox1.Height - 1;
+                }
+
+                Bitmap bitm = new Bitmap(m);
+                if (AlgorytmBresenhama == true)
+                {
+                    bitm = DrawLineBlack(bitm, X, Y, X1, Y1);
+                }
+                if (AlgorytmPrzyrostowy == true)
+                {
+                    bitm = Przyrostowy(bitm, X, Y, X1, Y1);
+                }
+                pictureBox1.Image = bitm;
+            }
+           
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            AlgorytmPrzyrostowy = true;
+            AlgorytmBresenhama = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Cleaked = false;
+            Bitmap m1 = new Bitmap(pictureBox1.Width,pictureBox1.Height);
+            if (AlgorytmBresenhama == true)
+            {
+                 m1 = DrawLineBlack(m, X, Y, X1, Y1);
+            }
+           
+            if (AlgorytmPrzyrostowy == true)
+            {
+                 m1 = Przyrostowy(m, X, Y, X1, Y1);
+            }
+             
+            pictureBox1.Image = m1;
+          
+        }
+        private Bitmap DrawLineBlack( Bitmap n ,int x1, int y1, int x2, int y2)
+        {
+            int deltax, deltay, krokx, kroky, błęd;
+            deltax = x2 - x1;
+            if (deltax > 0)
+            {
+                krokx = 1;
+            }
+            else
+            {
+                krokx = -1;
+            }              
+            deltax = Math.Abs(deltax);
+            deltay = y2 - y1;
+            if (deltay > 0)
+            {
+                kroky = 1;
+            }
+            else
+            {
+                kroky = -1;
+            }
+            deltay = Math.Abs(deltay);
+            if (deltax > deltay)
+            {
+
+                błęd = -deltax;
+                while (x1 != x2)
+                {                  
+                    n.SetPixel(x1, y1, System.Drawing.Color.Black);
+                    błęd += 2 * deltay;
+                    if (błęd > 0) 
+                    { 
+                        y1 += kroky;
+                        błęd -= 2 * deltax;
+                    }
+                    x1 += krokx;
+                }
+            }
+            else
+            {
+                błęd = -deltay;
+                while (y1 != y2)
+                {
+                    n.SetPixel(x1, y1, System.Drawing.Color.Black);
+
+                    błęd += 2 * deltax;
+                    if (błęd > 0) 
+                    { 
+                        x1 += krokx;
+                        błęd -= 2 * deltay; 
+                    }
+                    y1 += kroky;
+                }
+            }
+            return n;
+        }
+        private Bitmap Przyrostowy(Bitmap n, int x1, int y1, int x2, int y2)
+        {
+            int x;
+            int y;
+            float deltax, deltay,Y,X,m;           
+            deltax = x2 - x1;
+            deltay = y2 - y1;          
+            m = deltay / deltax;
+            Y = y1;
+            X = x1;
+            if (Math.Abs(m) >= 1)
+            {
+                for (y = y1; y <= y2; y++)
+                {
+                    n.SetPixel((int)Math.Floor(X + 0.5), y, Color.Red);
+                    X += (1 / m);
+                }
+            }
+            else
+            {
+                for (x = x1; x <= x2; x++)
+                {
+                    n.SetPixel(x, (int)Math.Floor(Y + 0.5), Color.Red);
+                    Y += m;
+                }
+            }
+            if ( deltay < 0 || deltax < 0)
+            {
+                if (Math.Abs(m) >= 1) 
+                {
+                    X = x2;
+                    for (y = y2; y <= y1; y++)
+                    {
+                        n.SetPixel((int)Math.Floor(X + 0.5), y, Color.Red);
+                        X += (1 / m);
+                    }
+                   
+                }
+                else
+                {
+                    Y = y2;
+                    for (x = x2; x <= x1; x++) 
+                    {
+                        n.SetPixel(x, (int)Math.Floor(Y + 0.5), Color.Red);
+                        Y += m;
+                    }
+                }
+            }
+          
+                
+            
+            return n;
+          
+        }
+
+
+    }
+}
